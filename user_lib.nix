778c95898a931c4dfb0834fb0fd5b444b7c5ab7a
Pierre Thibault <pierre.thibault@pierrethibault.dev>
2024-11-06 11:11:36 -0500

    Adding English, French and Spanish dictionaries.

--- user_lib.nix
+++ user_lib.nix
@@ -3,6 +3,15 @@
 let userdata = import ./userdata.nix; in
 {
   users.users.${userdata.username}.packages = with pkgs; [
+    hunspell
+    hunspellDicts.en-ca
+    hunspellDicts.en-gb-ise
+    hunspellDicts.en-us
+    hunspellDicts.fr-any
+    hunspellDicts.fr-classic
+    hunspellDicts.fr-moderne
+    hunspellDicts.es-mx
+    languagetool
   ];
 }
 
