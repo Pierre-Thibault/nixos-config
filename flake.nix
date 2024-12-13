24ff3ce491c77569f4335db60bb47330e830b3a0
Pierre Thibault <pierre.thibault@pierrethibault.dev>
2024-12-13 13:29:12 -0500

    Upgrading from 24.05 to 24.11.

--- flake.nix
+++ flake.nix
@@ -4,12 +4,14 @@
   description = "My NixOS's flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.pierre-nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+  outputs =
+    { self, nixpkgs }:
+    {
+      nixosConfigurations.pierre-nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
     };
-  };
 }
