d0cdb485544c91207bc586dc79e2a9980824c2c9
Pierre Thibault <pierre.thibault@pierrethibault.dev>
2025-04-22 22:37:26 -0400

    Pinning to last release of unstable.

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     }:
     let
       unstableTarball = fetchTarball {
-        url = "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz";
+        url = "https://releases.nixos.org/nixos/unstable/nixos-25.05pre787278.c11863f1e964/nixexprs.tar.xz";
         sha256 = "0a16swsbgnzr5j991ggq17fiyyfvcn434k624250b4rp9bdj83hx";
       };
       unstable = import unstableTarball { system = "x86_64-linux"; };
