68d0620234a09ad907738b014b4889a4ec4928bd
Pierre Thibault <pierre.thibault@pierrethibault.dev>
2025-11-21 23:39:57 -0500

    Group services in configuration.

--- configuration.nix
+++ configuration.nix
@@ -75,9 +75,44 @@ in
     LC_TIME = "fr_CA.UTF-8";
   };
 
-  # Enable the GNOME Desktop Environment (it is xserver but in reality it is Wayland).
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services = {
+    xserver = {
+      # Enable the GNOME Desktop Environment (it is xserver but in reality it is Wayland).
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+
+      # Configure keymap in X11
+      xkb = {
+        layout = "ca";
+        variant = "multix";
+      };
+    };
+
+    desktopManager.plasma6.enable = true;
+
+    # Enable CUPS to print documents.
+    printing.enable = true;
+
+    # Enable sound with pipewire.
+    pulseaudio.enable = false;
+    rtkit.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+    pcscd.enable = true;
+
+    # Enable the OpenSSH daemon.
+    openssh.enable = userdata.ssh_enable;
+  };
 
   environment.gnome.excludePackages = with pkgs; [
     epiphany # web browser
@@ -95,8 +130,6 @@ in
     }
   ];
 
-  services.desktopManager.plasma6.enable = true;
-
   programs.steam = {
     enable = true;
   };
@@ -110,36 +143,8 @@ in
     };
   };
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "ca";
-    variant = "multix";
-  };
-
   # Configure console keymap to use the xserver config
   console.useXkbConfig = true;
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${userdata.username} = {
@@ -152,7 +157,6 @@ in
     shell = pkgs.zsh;
   };
 
-  services.pcscd.enable = true;
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
@@ -174,11 +178,6 @@ in
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = userdata.ssh_enable;
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts =
