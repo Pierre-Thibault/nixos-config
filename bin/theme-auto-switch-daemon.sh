5345719291d19dff5b864c24df5855549f130543
Pierre Thibault <pierre.thibault@pierrethibault.dev>
2026-03-05 21:47:26 -0500

    theme-auto-switch-daemon.sh was replaced by a service.

--- bin/theme-auto-switch-daemon.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Daemon to check and switch theme every 5 minutes
-
-while true; do
-    /home/pierre/nixos-config/bin/theme-auto-switch.sh
-    sleep 300  # Check every 5 minutes
-done
